310b1226e4b1679bfbed64ebc5011c2c16e80899
Stefan Bürk <stefan@buerk.tech>
2025-04-30 12:40:39 +0200

    [TASK] Streamline image setup
    This change contains a couple of changes, gathered by local
    builds and verifications and are now in one step across both
    images.
    
    Includes ...
    
    * Streamlining and bringing the `entrypoint.sh` into place to
      add `typo3` user/group on service startup and `chown'ing`
      files.
    * Overwork `supervisord` configuration using sane defaults and
      split services in dedicated files with the correct options.
    * Use correct condition for regexp matching within the apache2
      envvars files to correctly prefix user/group id's with the
      mandantory hashtag (`#`).

--- typo3-base-apache2-phpfpm74/entrypoint.sh
+++ typo3-base-apache2-phpfpm74/entrypoint.sh
@@ -1,21 +1,33 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 if [[ "$1" == "/usr/bin/supervisord" ]]; then
   echo '>> Changing user/group for files'
   APP_RUN_USER="${APP_RUN_USER:-www-data}"
   APP_RUN_GROUP="${APP_RUN_GROUP:-www-data}"
   PROJECT_FOLDER="${PROJECT_FOLDER:-/var/www/html/project}"
-  addgroup typo3 --gid=${APP_RUN_GROUP};
-  adduser --home ${PROJECT_FOLDER} --shell /bin/bash --no-create-home --disabled-login --ingroup typo3 --gecos GECOS --uid ${APP_RUN_USER} typo3;
-  chown -R ${APP_RUN_USER}:${APP_RUN_GROUP} ${PROJECT_FOLDER}
-  chown -R ${APP_RUN_USER}:${APP_RUN_GROUP} /var/www/html
+  if [ $(getent group typo3) ]; then
+    echo "group 'typo3' exists."
+  else
+    echo "group 'typo3' does not exist."
+    addgroup typo3 --gid=${APP_RUN_GROUP};
+  fi
+  if [ $(getent passwd typo3) ]; then
+    echo "user 'typo3' exists."
+  else
+    echo "user 'typo3' does not exist."
+    adduser --home ${PROJECT_FOLDER} --shell /bin/bash --no-create-home --disabled-login --ingroup typo3 --gecos GECOS --uid ${APP_RUN_USER} typo3
+    echo -n "change user/group for folders ..."
+    chown -R --silent ${APP_RUN_USER}:${APP_RUN_GROUP} ${PROJECT_FOLDER}
+    chown -R --silent ${APP_RUN_USER}:${APP_RUN_GROUP} /var/www/html
+    echo "done"
+  fi
   exec "$@"
 fi
 
 if [[ "$( id -u )" -eq 0 ]]; then
   # su behaves inconsistently with -c followed by flags
   # Workaround: run the entrypoint and commands as a standalone script
-  echo "#!/usr/bin/env bash" > /usr/local/bin/invocation.sh
+  echo "#!/bin/bash" > /usr/local/bin/invocation.sh
   echo >> /usr/local/bin/invocation.sh
   for ARG in "$@"; do
       printf "${ARG} " >> /usr/local/bin/invocation.sh
