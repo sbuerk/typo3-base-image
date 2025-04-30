5e9e2c780e03ad2f87026b650303bb1974b4454f
Stefan Bürk <stefan@buerk.tech>
2025-04-30 02:21:32 +0200

    [TASK] Use chown instead of chmod in `entrypoint.sh`

--- typo3-base-apache2-phpfpm72/entrypoint.sh
+++ typo3-base-apache2-phpfpm72/entrypoint.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 if [[ "$1" == "/usr/bin/supervisord" ]]; then
+  echo '>> Changing user/group for files'
   APP_RUN_USER="${APP_RUN_USER:-www-data}"
   APP_RUN_GROUP="${APP_RUN_GROUP:-www-data}"
   PROJECT_FOLDER="${PROJECT_FOLDER:-/var/www/html/project}"
-  chmod -R ${APP_RUN_USER}:${APP_RUN_GROUP} ${PROJECT_FOLDER}
-  chmod -R ${APP_RUN_USER}:${APP_RUN_GROUP} ${PROJECT_FOLDER}
+  chown -R ${APP_RUN_USER}:${APP_RUN_GROUP} ${PROJECT_FOLDER}
+  chown -R ${APP_RUN_USER}:${APP_RUN_GROUP} /var/www/html
 fi
 
 exec "$@"
