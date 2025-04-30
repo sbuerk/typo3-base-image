0f898bb478354973aa301f6baa16720b19b23d96
Stefan Bürk <stefan@buerk.tech>
2025-04-30 03:30:47 +0200

    [TASK] Add user/group creation in entrypoint and dispatch to user for uid=0

--- typo3-base-apache2-phpfpm74/entrypoint.sh
+++ typo3-base-apache2-phpfpm74/entrypoint.sh
@@ -7,6 +7,13 @@ if [[ "$1" == "/usr/bin/supervisord" ]]; then
   PROJECT_FOLDER="${PROJECT_FOLDER:-/var/www/html/project}"
   chown -R ${APP_RUN_USER}:${APP_RUN_GROUP} ${PROJECT_FOLDER}
   chown -R ${APP_RUN_USER}:${APP_RUN_GROUP} /var/www/html
+  addgroup typo3 --gid=${APP_RUN_GROUP}
+  adduser -h ${PROJECT_FOLDER} -D -G typo3 --uid=${APP_RUN_USER} typo3;
+  exec "$@"
 fi
 
-exec "$@"
+if [[ "$( id -u )" -eq 0 ]]; then
+  su - typo3 -c "/usr/local/bin/entrypoint.sh $@"
+else
+  exec "$@"
+fi
