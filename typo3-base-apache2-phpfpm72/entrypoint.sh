98aa3d1b127889ec16b5213f246f17cc888eb988
Stefan Bürk <stefan@buerk.tech>
2025-04-30 10:12:37 +0200

    [TASK] Use pseudo terminal flag for internal invocation dispatch

--- typo3-base-apache2-phpfpm72/entrypoint.sh
+++ typo3-base-apache2-phpfpm72/entrypoint.sh
@@ -6,26 +6,24 @@ if [[ "$1" == "/usr/bin/supervisord" ]]; then
   APP_RUN_GROUP="${APP_RUN_GROUP:-www-data}"
   PROJECT_FOLDER="${PROJECT_FOLDER:-/var/www/html/project}"
   addgroup typo3 --gid=${APP_RUN_GROUP};
-  adduser --home ${PROJECT_FOLDER} --no-create-home --disabled-login --ingroup typo3 --gecos GECOS --uid ${APP_RUN_USER} typo3;
+  adduser --home ${PROJECT_FOLDER} --shell /bin/bash --no-create-home --disabled-login --ingroup typo3 --gecos GECOS --uid ${APP_RUN_USER} typo3;
   chown -R ${APP_RUN_USER}:${APP_RUN_GROUP} ${PROJECT_FOLDER}
   chown -R ${APP_RUN_USER}:${APP_RUN_GROUP} /var/www/html
   exec "$@"
 fi
 
 if [[ "$( id -u )" -eq 0 ]]; then
-  echo "Is root, dispatch entrypoint again with user typo3(${APP_RUN_USER})"
-  su --group typo3 - typo3 -c "/usr/local/bin/entrypoint.sh $@"
-
   # su behaves inconsistently with -c followed by flags
   # Workaround: run the entrypoint and commands as a standalone script
   echo "#!/usr/bin/env bash" > /usr/local/bin/invocation.sh
   echo >> /usr/local/bin/invocation.sh
-  for ARG in "${$@}"; do
+  for ARG in "$@"; do
       printf "${ARG} " >> /usr/local/bin/invocation.sh
   done
   chmod a+x /usr/local/bin/invocation.sh
-  su - typo3 -c "/usr/local/bin/invocation.sh"
+
+  su typo3 -l -P -c "/usr/local/bin/invocation.sh"
+
 else
-  echo "Simply dispatch command $@ as root: $( id -u )"
   exec "$@"
 fi
